76fb50008b5f77426c79b8dd5aa184805dd954fc
GANESH KUMAR <ganeshplays700@gmail.com>
2026-03-22 21:38:32 +0530

    Add more projects to portfolio

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -33,10 +33,44 @@ export const allProjects = [
     title: "University Management System",
     description:
       "Developed a full-stack university management system to streamline administrative workflows and enhance student services. Designed the system to support scalable management of academic processes and improve overall efficiency.",
-    stack: ["React", "Node", "MongoDB", "Tailwind CSS"],
+    stack: ["React", "Node", "MongoDB", "REST API"],
     image: "/erp.png", // You can replace with the actual project image
     github:
       "https://github.com/codewithganeshhh/College-CMS",
+  },
+  {
+    title: "BestBite",
+    description:
+      "BestBite is a food delivery website that allows users to order food from local restaurants. It features a modern UI design with a seamless user experience, enabling users to browse menus, place orders, and track their delivery status.",
+    stack: ["HTML", "Bootstrap", "JavaScript"],
+    image: "/bestbite.png", // You can replace with the actual project image
+    github:
+      "https://github.com/codewithganeshhh/BestBites",
+    demo:
+      "https://best-bites-dusky.vercel.app/",
+  },
+  {
+    title: "NSU Tech Club",
+    description:
+      "NSU Tech Club is a  website for Netaji Subhas University's official tech club. It promotes learning, innovation, and collaboration in technology through workshops, hackathons, and real-world projects. ",
+    stack: ["HTML", "Bootstrap", "Web3Form API"],
+    image: "/techclub.png", // You can replace with the actual project image
+    github:
+      "https://github.com/codewithganeshhh/NSU-TECH-CLUB",
+    demo:
+      "https://codewithganeshhh.github.io/NSU-TECH-CLUB/",
+  },
+
+  {
+    title: "Dynamic Weather Dashboard",
+    description:
+      "A dynamic weather dashboard that displays real-time weather information for any city worldwide. It features enabling users to check weather conditions, forecasts, and more.",
+    stack: ["JavaScript", "Bootstrap", "Weather API"],
+    image: "/weather.png", // You can replace with the actual project image
+    github:
+      "https://github.com/codewithganeshhh/Dynamic-Weather-Dashboard",
+    demo:
+      "https://codewithganeshhh.github.io/Dynamic-Weather-Dashboard/",
   },
 ];
 
